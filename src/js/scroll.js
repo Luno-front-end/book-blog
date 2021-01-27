097ac79527d200094cd661a0fee300cb3e8cc2c6
Luno-front-end <63469409+Luno-front-end@users.noreply.github.com>
2021-01-28 01:46:37 +0200

    add warkup and min styles

---
+++ src/js/scroll.js
@@ -0,0 +1,10 @@
+import animateScrollTo from 'animated-scroll-to';
+const btnEl = document.getElementById('btnScroll');
+const sectionEl = document.getElementById('section-book');
+
+btnEl.addEventListener('click', scrollFn);
+
+function scrollFn(e) {
+  console.log(e.currentTarget);
+  animateScrollTo(sectionEl);
+}
